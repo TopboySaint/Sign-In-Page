021272cad92939fe1bf10bbc8cd64cc04d8892ee
TopboySaint <topboysaint1@gmail.com>
2024-05-20 12:33:18 +0100

    Saintly Done

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ const submitDetails = () => {
     // alert('yes i am working')
     if(matricNo.value == "" || passwordKey.value == ""){
         document.getElementById('errorMsg').style.display = "block"
+    }else{
+        document.getElementById('errorMsg').style.display = "none"
+        document.getElementById('successMsg').style.display = "block"
+    
     }
 }
